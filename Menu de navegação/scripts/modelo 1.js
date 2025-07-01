165ca2c9842a41ac7ee6e84f9038bd34a1956a7a
Wevertonf45 <weverton07102005@gmail.com>
2025-07-01 17:37:22 -0300

    Novo menu de navegaçao

---
+++ Menu de navegação/scripts/modelo 1.js
@@ -0,0 +1,11 @@
+let menul = document.getElementById('menu-l')
+let fechar = document.getElementById('menu-l')
+
+
+function abrirMenu(){
+  menul.style.display = 'flex'
+}
+
+function fecharMenu(){
+  fechar.style.display = 'none'
+}
